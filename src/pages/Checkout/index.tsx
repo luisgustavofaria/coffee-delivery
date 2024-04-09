53bfb26544841fc698e4c3eaca8dfc39a0d6e1ea
Luis <luis.gustavo.fariac@gmail.com>
2024-04-09 19:25:42 -0300

    criado Router

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../../components/Container/styles';
+import { Header } from '../../components/Header';
+
+export function Checkout() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
