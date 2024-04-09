7874bd3cae7a7121e56ca10617bff9e22be8300e
Luis <luis.gustavo.fariac@gmail.com>
2024-04-09 20:08:53 -0300

    criado NavLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,23 @@
 import { MapPin, ShoppingCart } from '@phosphor-icons/react';
 import { Aside, ContainerHeader } from './styles';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
     <ContainerHeader>
-      <img src="/images/logo.svg" alt="logo" />
+      <NavLink to={'/'}>
+        <img src="/images/logo.svg" alt="logo" />
+      </NavLink>
       <Aside>
         <div>
           <MapPin size={22} color="#8047F8" weight="duotone" />
           <span>Porto Alegre, RS</span>
         </div>
-        <div>
-          <ShoppingCart size={22} weight="fill" color="#C47F17" />
-        </div>
+        <NavLink to="/checkout">
+          <div>
+            <ShoppingCart size={22} weight="fill" color="#C47F17" />
+          </div>
+        </NavLink>
       </Aside>
     </ContainerHeader>
   );
